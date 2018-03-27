793ccd0c8cf850946d9d6cb20d4c0234ccaa2846
Pham Phu Quoc <quoc.pham@banvien.com.vn>
2018-03-27 11:48:20 +0700

    added

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='Odoo',
       version='8.0',
       description='OpenShift deployment quickstart for Odoo',
-      author='Daniel Reis',
-      url='https://github.com/dreispt/openshift-odoo-quickstart/',
+      author='Quoc Pham',
+      url='https://github.com/quocpp/odooTest',
      )
